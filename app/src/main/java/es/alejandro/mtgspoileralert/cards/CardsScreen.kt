e8feda53653a577f5fead9f1069278c251c70d34
Alejandro Rojas Pérez <al.rojas.perez@gmail.com>
2022-05-11 17:10:35 +0200

    Added long click on cards

--- app/src/main/java/es/alejandro/mtgspoileralert/cards/CardsScreen.kt
+++ app/src/main/java/es/alejandro/mtgspoileralert/cards/CardsScreen.kt
@@ -1,23 +1,25 @@
 package es.alejandro.mtgspoileralert.cards
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Card
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.annotation.ExperimentalCoilApi
 import coil.compose.rememberImagePainter
@@ -59,9 +61,10 @@ fun CardsScreen(
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun CardsList(cards: List<Card>, onCardClick: (String) -> Unit) {
-    LazyColumn {
+    LazyVerticalGrid(cells = GridCells.Fixed(2)) {
         items(cards) { item ->
             SingleCardItem(item) { cardId ->
                 onCardClick(cardId)
@@ -70,16 +73,27 @@ fun CardsList(cards: List<Card>, onCardClick: (String) -> Unit) {
     }
 }
 
-@OptIn(ExperimentalCoilApi::class)
+@OptIn(ExperimentalCoilApi::class, ExperimentalFoundationApi::class)
 @Composable
 fun SingleCardItem(
     card: Card,
     onClick: (String) -> Unit
 ) {
+
+    var showDialog by remember { mutableStateOf(false) }
+    if (showDialog) {
+        DialogCard(card.image_uris.normal) {
+            showDialog = false
+        }
+    }
+
     Card(modifier = Modifier
         .padding(8.dp)
         .fillMaxWidth()
-        .clickable { onClick(card.id) }, elevation = 8.dp) {
+        .combinedClickable(
+            onClick = { onClick(card.id) },
+            onLongClick = { showDialog = true }
+        ), elevation = 8.dp) {
         Image(
             modifier = Modifier.fillMaxSize(), painter = rememberImagePainter(
                 card.image_uris.normal,
@@ -87,5 +101,16 @@ fun SingleCardItem(
                     size(OriginalSize)
                 }
             ), contentDescription = null, contentScale = ContentScale.Crop)
+    }
+}
+
+@OptIn(ExperimentalCoilApi::class)
+@Composable
+fun DialogCard(imageUri: String, onClose: () -> Unit){
+    Dialog(
+        onDismissRequest = onClose,
+        DialogProperties(dismissOnBackPress = true, dismissOnClickOutside = true)
+    ) {
+        Image(painter = rememberImagePainter(data = imageUri), contentDescription = null)
     }
 }
