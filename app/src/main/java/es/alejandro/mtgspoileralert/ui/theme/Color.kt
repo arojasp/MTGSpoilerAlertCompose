c561bfa45294b9a1b38a0d4d127286ca9ecfc4d0
Alejandro Rojas <al.rojas.perez@gmail.com>
2022-05-12 00:27:19 +0200

    Added primary and secondary colors

--- app/src/main/java/es/alejandro/mtgspoileralert/ui/theme/Color.kt
+++ app/src/main/java/es/alejandro/mtgspoileralert/ui/theme/Color.kt
@@ -1,8 +1,16 @@
 package es.alejandro.mtgspoileralert.ui.theme
 
+import androidx.compose.material.darkColors
 import androidx.compose.ui.graphics.Color
 
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val Indigo300 = Color(0xFF7986CB)
+val Indigo300Dark = Color(0xFF49599A)
+val Indigo300Light = Color(0xFFAAB6FE)
+val DeepPurple300 = Color(0xFF9575CD)
+val DeepPurple300Dark = Color(0xFF65499C)
+val DeepPurple300Light = Color(0xFFC7A4FF)
