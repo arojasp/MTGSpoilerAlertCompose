88456e16b70cf14e2c551a8b157592f4d9ebbdf2
Alejandro Rojas Pérez <al.rojas.perez@gmail.com>
2022-05-19 09:01:04 +0200

    :wip: started with design

--- app/src/main/java/es/alejandro/mtgspoileralert/datastore/SettingsDataStoreManager.kt
+++ app/src/main/java/es/alejandro/mtgspoileralert/datastore/SettingsDataStoreManager.kt
@@ -1,12 +1,10 @@
 package es.alejandro.mtgspoileralert.datastore
 
 import android.content.Context
-import androidx.datastore.preferences.core.booleanPreferencesKey
-import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.core.intPreferencesKey
-import androidx.datastore.preferences.core.longPreferencesKey
+import androidx.datastore.preferences.core.*
 import androidx.datastore.preferences.preferencesDataStore
 import dagger.hilt.android.qualifiers.ApplicationContext
+import es.alejandro.mtgspoileralert.settings.model.Settings
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import java.util.concurrent.TimeUnit
@@ -20,10 +18,31 @@ class SettingsDataStoreManager @Inject constructor(@ApplicationContext appContex
         val CORE_LISTEN = booleanPreferencesKey("core_listen")
         val COMMANDER_LISTEN = booleanPreferencesKey("core_listen")
         val INTERVAL = intPreferencesKey("interval")
-        val TIME_UNIT = longPreferencesKey("time_unit")
+        val TIME_UNIT = stringPreferencesKey("time_unit")
     }
 
     private val settingsDataStore = appContext.dataStore
+
+    suspend fun setSettings(settings: Settings) {
+        settingsDataStore.edit { settingsPreferences ->
+            settingsPreferences[CORE_LISTEN] = settings.coreSetListen
+            settingsPreferences[COMMANDER_LISTEN] = settings.commanderSetListen
+            settingsPreferences[INTERVAL] = settings.interval.first
+            settingsPreferences[TIME_UNIT] = settings.interval.second.toString()
+        }
+    }
+
+    val settings: Flow<Settings> = settingsDataStore.data.map { settingsPreferences ->
+        Settings(
+            coreSetListen = settingsPreferences[CORE_LISTEN] ?: false,
+            commanderSetListen = settingsPreferences[COMMANDER_LISTEN] ?: false,
+            interval = Pair(
+                settingsPreferences[INTERVAL] ?: 15,
+                TimeUnit.valueOf(settingsPreferences[TIME_UNIT] ?: TimeUnit.MINUTES.toString())
+            )
+        )
+
+    }
 
     suspend fun setCoreListening(isListening: Boolean) {
         settingsDataStore.edit { settings ->
